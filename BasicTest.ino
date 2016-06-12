c9d4a19836a23a6faabe11ee6d22583e7ba20b17
cconstantine <cconstan@gmail.com>
2016-06-12 09:22:30 -0700

    serial

--- BasicTest.ino
+++ BasicTest.ino
@@ -9,10 +9,10 @@ struct Packet {
   uint8_t data[60];
 };
 
-const int ledsPerStrip = 4*60;
+const int ledsPerStrip = 4 * 60;
 
-DMAMEM uint8_t displayMemory[ledsPerStrip*8*3];
-uint8_t drawingMemory[ledsPerStrip*8*3];
+DMAMEM uint8_t displayMemory[ledsPerStrip * 8 * 3];
+uint8_t drawingMemory[ledsPerStrip * 8 * 3];
 
 const int config = WS2811_GRB | WS2811_800kHz;
 
@@ -20,20 +20,40 @@ OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
 
 void setup() {
   Serial.begin(9600); // USB is always 12 Mbit/sec
-
+  Serial.setTimeout(0);
   leds.begin();
+
+  for (int i = 0; i < leds.numPixels(); i++) {
+    leds.setPixel(i, 0x0f000F);
+  }
   leds.show();
 }
 
 void loop() {
+  static unsigned long bytes = 0;
+  static unsigned long lastMillis = millis();
+  if (lastMillis + 1000 < millis()) {
+    lastMillis += 1000;
+
+    Serial.print("Transfered: ");Serial.println(bytes);
+    bytes = 0;
+  }
   Packet packet;
-  int n = RawHID.recv(&packet, 0);
-  if (n > 0) {
-    for(int i = 0;i < packet.pixel_count*3;i+=3) {
-      int idx = i/3 + packet.pixel_index + packet.strip_index*ledsPerStrip;
-      leds.setPixel(idx, packet.data[i], packet.data[i+1], packet.data[i+2]);
+  if (Serial.available() < sizeof(packet)) {
+    return;
+  }
+  int n = Serial.readBytes((char*)&packet, sizeof(packet));// RawHID.recv(&packet, 0);
+  if (n > 0 ) {
+    bytes += n;
+
+    for (int i = 0; i < packet.pixel_count; i++) {
+      int idx = i + packet.pixel_index + packet.strip_index * ledsPerStrip;
+      leds.setPixel(idx, packet.data[i * 3], packet.data[i * 3 + 1], packet.data[i * 3 + 2]);
     }
-    leds.show();
+
+    if (packet.flags & 0x01 && !leds.busy()) {
+      leds.show();
+    }
   }
 }
 
